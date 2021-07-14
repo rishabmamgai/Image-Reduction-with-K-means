249a6a24b28880389dc8c798214ce42a148307e2
Rishab Mamgai <61201838+rishabmamgai@users.noreply.github.com>
2021-07-14 13:35:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ X = data.load_data(file)
 
 K = 3
 iters = 50
-#final_centroids, final_cluster_loc, final_distortion = run_Kmeans(X, K, iters)
-
-#print(final_distortion)
-#plot_clusters(X, final_cluster_loc, final_centroids)
-
 
 pixel_mat = get_pixel_mat("image\original.jpg")
 final_centroids, final_cluster_loc, final_distortion = Kmeans(pixel_mat, K, iters)
